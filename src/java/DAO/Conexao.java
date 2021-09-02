05a9b273fd41eea7405337ab5b659726b9f5c8d9
Matheus Augusto Garcia <matheusgxd@gmail.com>
2021-09-02 15:44:18 -0300

    add files

---
+++ src/java/DAO/Conexao.java
@@ -0,0 +1,24 @@
+package DAO;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+    
+    public Connection getConexao() {
+        
+        try {
+            
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            return DriverManager.getConnection("jdbc:derby://localhost/bdTreino","root","123");
+            
+        }
+        
+        catch(Exception erro) {
+            
+            throw new RuntimeException("Erro classe conexão: ",erro);
+            
+        }
+        
+    }
+    
+}
---
+++ src/java/DAO/DAOCliente.java
@@ -0,0 +1,41 @@
+package DAO;
+import MODEL.Cliente;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class DAOCliente {
+    
+    private Connection conn;
+    private PreparedStatement stmt;
+    
+    public DAOCliente() {
+        
+        conn = new Conexao().getConexao();
+        
+    }
+    
+    //metodo inserir cliente
+    
+    public void inserirCliente(Cliente cliente) {
+        
+        String sql = "INSERT INTO TB_CLIENTES(nome,email)"+"VALUES(?,?)";
+        
+        try {
+            
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1,cliente.getNome());
+            stmt.setString(2,cliente.getEmail());
+            stmt.execute();
+            stmt.close();
+            
+        } 
+        
+        catch (Exception erro) {
+            
+            throw new RuntimeException("Erro Inserir Cliente: ", erro);
+            
+        }
+        
+    } 
+    
+}
